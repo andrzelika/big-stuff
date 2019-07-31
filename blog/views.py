1528686064442ead49ad12973e13d618fcf7daae
andrzelika <smiechfetka@gmail.com>
2019-07-31 21:48:57 +0200

    Widok listy i szczegółów wpisu plus dodanie szablonów - models.py, views.py, urls.py, templates, .html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 # views.py: w tym pliku znajduje się logika aplikacji. Każdy widok otrzymuje
 # żądanie HTTP, przetwarza je i zwraca odpowiedź.
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+	posts = Post.published.all() 
+	# używamy wczesniej utworzonego w models.py menagera published
+
+	return render(request, 
+				  'blog/post/list.html', 
+				  {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug = post,
+								   status = 'published',
+								   publish__year = year,
+								   publish__month = month,
+								   pyblish__day = day)
+	return render(request,
+				  'blog/post/detail.html',
+				  {'post': post})
